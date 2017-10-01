12fa8e67ab4952d086f54e0bbb231464ff932597
Daniel Zlotin <danielzl@wix.com>
2017-10-01 17:14:56 +0300

    migrate to 0.44

--- ios/react_native_navigation_bootstrap/AppDelegate.m
+++ ios/react_native_navigation_bootstrap/AppDelegate.m
@@ -1,17 +1,14 @@
 #import "AppDelegate.h"
 #import "RCCManager.h"
 #import <React/RCTRootView.h>
+#import <React/RCTBundleURLProvider.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
-#ifdef DEBUG
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-#else
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
   
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
